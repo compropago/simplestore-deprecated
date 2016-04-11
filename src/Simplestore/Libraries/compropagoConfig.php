91453083635bfbc3405d56f8a1b827dd5bbc1023
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2016-04-11 12:17:20 -0500

    Add Contained in config and define version const

--- src/Simplestore/Libraries/compropagoConfig.php
+++ src/Simplestore/Libraries/compropagoConfig.php
@@ -8,17 +8,18 @@
  * @author Eduardo Aguilar <eduardo.aguilar@compropago.com>
  */
 
+define("CP_ST_VERSION", "1.0.1");
 
 /**
  * @var array General Configurations for SDK
  */
 $compropagoConfig = array(
     # Llave publica generada por compropago
-    'publickey'=>'pk_test_XXXXXXXXXXXXXXXXX',
-
+    'publickey'=>'pk_test_8781245a88240f9cf',
     # Llave privada gerada por compropago
-    'privatekey'=>'sk_test_XXXXXXXXXXXXXXXXX',
-
+    'privatekey'=>'sk_test_56e31883637446b1b',
     # Estas generando pruebas? si no es asi cambia el valor a 'true'
-    'live'=>false
+    'live'=>false,
+    # Definicion estatica de version, (No modificar),
+    'contained' => 'plugin; cpst '. CP_ST_VERSION .' ;'
 );
